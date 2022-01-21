7e46f3f55ec4e9901a782bd42a9aa3811da7e9de
chansaem <ased1233@naver.com>
2022-01-21 17:41:17 +0900

    slider_bar

--- src/Routes/Home.tsx
+++ src/Routes/Home.tsx
@@ -1,3 +1,5 @@
+import { AnimatePresence, motion } from "framer-motion";
+import { useState } from "react";
 import { useQuery } from "react-query";
 import styled from "styled-components";
 import { getMovie, IGetMoviesResult } from "../api";
@@ -29,21 +31,85 @@ const Overview = styled.p`
   font-size: 25px;
   width: 50%;
 `;
+
+const Silder = styled.div`
+  position: relative;
+  top: -100px;
+`;
+const Row = styled(motion.div)`
+  display: grid;
+  gap: 5px;
+  grid-template-columns: repeat(6, 1fr);
+  position: absolute;
+  width: 100%;
+`;
+const Box = styled(motion.div)<{ bgPhoto: string }>`
+  background-color: white;
+  height: 200px;
+  color: red;
+  font-size: 30px;
+  background-image: url(${(props) => props.bgPhoto});
+  background-size: cover;
+  background-position: center center;
+`;
+const rowVar = {
+  hidden: { x: window.innerWidth - 10 },
+  visible: { x: 0 },
+  exit: { x: -window.innerWidth + 10 },
+};
+const offset = 6;
 function Home() {
   const { data, isLoading } = useQuery<IGetMoviesResult>(
     ["movies", "nowPlaying"],
     getMovie
   );
+  const [index, setIndex] = useState(0);
+  const [leaving, setLeaving] = useState(false);
+  const incraseIndex = () => {
+    if (data) {
+      if (leaving) return;
+      ToggleLeaving();
+      const totalMovie = data.results.length - 1;
+      const maxIndex = Math.floor(totalMovie / offset) - 1;
+      setIndex((prev) => (prev === maxIndex ? 0 : prev + 1));
+    }
+  };
+  const ToggleLeaving = () => setLeaving((prev) => !prev);
   return (
     <Wrapper>
       {isLoading ? (
         <Loading></Loading>
       ) : (
         <>
-          <Banner bgPhoto={makeImagePath(data?.results[0].backdrop_path || "")}>
-            <Title>{data?.results[0].original_title}</Title>
-            <Overview>{data?.results[0].overview}</Overview>
+          <Banner
+            onClick={incraseIndex}
+            bgPhoto={makeImagePath(data?.results[1].backdrop_path || "")}
+          >
+            <Title>{data?.results[1].original_title}</Title>
+            <Overview>{data?.results[1].overview}</Overview>
           </Banner>
+          <Silder>
+            <AnimatePresence initial={false} onExitComplete={ToggleLeaving}>
+              <Row
+                variants={rowVar}
+                initial="hidden"
+                animate="visible"
+                transition={{ type: "tween", duration: 2 }}
+                exit="exit"
+                key={index}
+              >
+                {data?.results
+                  .slice(1)
+                  .slice(offset * index, offset * index + offset)
+                  .map((movie) => (
+                    <Box
+                      key={movie.Id}
+                      bgPhoto={makeImagePath(movie.backdrop_path, "w500")}
+                    />
+                  ))}
+              </Row>
+            </AnimatePresence>
+          </Silder>
         </>
       )}
     </Wrapper>
