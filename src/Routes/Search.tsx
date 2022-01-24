21dbcfb14bc4b1994c3bc850002fb8fa55f7abcf
chansaem <ased1233@naver.com>
2022-01-24 17:52:08 +0900

    box

--- src/Routes/Search.tsx
+++ src/Routes/Search.tsx
@@ -2,6 +2,7 @@ import styled from "styled-components";
 
 const Title = styled.span`
   font-size: 200px;
+  color: red;
 `;
 
 function Search() {
--- src/Routes/Tv.tsx
+++ src/Routes/Tv.tsx
@@ -3,8 +3,13 @@ import styled from "styled-components";
 const Title = styled.span`
   font-size: 200px;
 `;
+const Box = styled.div`
+  background-color: red;
+  height: 100px;
+  width: 100px;
+`;
 function Tv() {
-  return <Title>asldklwkldkwlllllll</Title>;
+  return <Box></Box>;
 }
 
 export default Tv;
